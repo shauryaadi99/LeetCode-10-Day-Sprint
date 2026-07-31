33d681a39d02e1324e20d585353284c824749ec9
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-08-01 00:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (29.71%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -1,43 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
-public: // in a bst inorder taversal of a binaray seach tree is always sorted
-    vector<int> arr;
-    void inorder(TreeNode* root) {
-        if (!root)
-            return;
-        inorder(root->left);
-        arr.push_back(root->val);
-        inorder(root->right);
-    }
+public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        inorder(root);
-
-        int x = -1, y = -1;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] >= low) {
-                x = i;
-                break;
-            }
+        if (!root) {
+            return 0;
         }
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (arr[i] <= high) {
-                y = i;
-                break;
-            }
-        }
-        if (x == -1 || y == -1)
-            return -1;
-        return accumulate(arr.begin() + x, arr.begin() + y + 1, 0);
+        
+        int currentVal = (root->val >= low && root->val <= high) ? root->val : 0;
+        
+        int leftSum = rangeSumBST(root->left, low, high);
+        int rightSum = rangeSumBST(root->right, low, high);
+        
+        return currentVal + leftSum + rightSum;
     }
 };
