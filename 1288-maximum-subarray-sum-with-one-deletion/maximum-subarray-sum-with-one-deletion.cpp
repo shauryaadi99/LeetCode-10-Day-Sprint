d4fb769c79bbd0664f557b5dd8eddb44d7ee3519
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-08-15 22:13:02 +0530

    Time: 31 ms (5.01%) | Memory: 38.5 MB (5.16%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<int> arr;
-    int solve(int i, int j, vector<vector<int>>& dp) {
-        if (i == 0) {
-            return arr[0];
-        }
-        if (dp[i][j] != INT_MIN)
-            return dp[i][j];
-        // take
-        int x = max(arr[i], arr[i] + solve(i - 1, j, dp));
-        // not-take
-        int y = INT_MIN;
-        if (j > 0)
-            y = solve(i - 1, j - 1, dp);
-        dp[i][j] = max(x, y);
-        return dp[i][j];
-    }
     int maximumSum(vector<int>& arr) {
         int n = arr.size(), k = 1;
-        this->arr = arr;
         vector<vector<int>> dp(n, vector<int>(k + 1, INT_MIN));
+        for (int j = 0; j <= k; j++) {
+            dp[0][j] = arr[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                int x = max(arr[i], arr[i] + dp[i - 1][j]);
+                int y = INT_MIN;
+                if (j > 0)
+                    y = dp[i - 1][j - 1];
+                dp[i][j] = max(x, y);
+            }
+        }
         int ans = INT_MIN;
         for (int i = 0; i < n; i++) {
-            ans = max(ans, solve(i, k, dp));
+            ans = max(ans, dp[i][k]);
         }
         return ans;
     }
