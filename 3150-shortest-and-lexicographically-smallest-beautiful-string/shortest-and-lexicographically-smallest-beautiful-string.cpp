d6c69aac0e0faf85f0bdb56a88ee0a0d47aaa986
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-08-26 17:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.74%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -5,37 +5,25 @@ public:
         int l = 0;
         int cnt1 = 0;
         string ans = "";
-
         for (int r = 0; r < n; r++) {
 
-            // Add s[r]
             if (s[r] == '1')
                 cnt1++;
 
-            // Too many 1s
+            int sz = r - l + 1;
             while (cnt1 > k) {
                 if (s[l] == '1')
                     cnt1--;
                 l++;
             }
-
-            // Exactly k 1s
             if (cnt1 == k) {
-
-                // Remove unnecessary leading zeroes
-                while (s[l] == '0')
+                while (s[l] == '0') {
                     l++;
-
-                int sz = r - l + 1;
-                string cur = s.substr(l, sz);
-
-                // Shorter is better.
-                // If same length, lexicographically smaller is better.
-                if (ans == "" ||
-                    sz < ans.size() ||
-                    (sz == ans.size() && cur < ans)) {
-                    ans = cur;
                 }
+                sz = r - l + 1;
+                if (ans == "" || sz < ans.size() ||
+                    (sz == ans.size() && s.substr(l, sz) < ans))
+                    ans = s.substr(l, sz);
             }
         }
 
