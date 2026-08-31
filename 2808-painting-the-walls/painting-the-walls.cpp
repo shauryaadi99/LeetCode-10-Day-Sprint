9833c00388d5e953bf4de901b7934300330bdc9b
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-08-31 20:33:27 +0530

    Time: 103 ms (18.34%) | Memory: 123 MB (11.27%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -19,7 +19,23 @@ public:
     }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        memset(t, -1, sizeof(t));
-        return solve(0, n, cost, time, n);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int remain = 1; remain <= n; remain++) {
+            dp[n][remain] = INF;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int remain = 1; remain <= n; remain++) {
+                int newremain = max(0, remain - 1 - time[i]);
+                int take = cost[i] + dp[i + 1][newremain];
+                int nottake = dp[i + 1][remain];
+                dp[i][remain] = min(take, nottake);
+            }
+        }
+        // memset(t, -1, sizeof(t));
+        // return solve(0, n, cost, time, n);
+        return dp[0][n];
     }
 };
