434b0896bd23ede7e2bcc08bd33a3a0f8505968e
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-09-03 14:41:36 +0530

    Time: 50 ms (27.60%) | Memory: 48.7 MB (45.44%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
     const int INF = 1e9;
-    int t[13][10001];
-    int solve(int i, int amount, vector<int>& coins) {
+    // int t[13][10001];
+    // int solve(int i, int amount, vector<int>& coins) {
 
-        if (amount == 0) {
-            return 0;
-        }
+    //     if (amount == 0) {
+    //         return 0;
+    //     }
 
-        if (i == 0) {
-            if (amount % coins[0] == 0) {
-                return amount / coins[0];
-            }
-            return INF;
-        }
-        if (t[i][amount] != -1)
-            return t[i][amount];
-        int take = INF;
-        int nottake = solve(i - 1, amount, coins);
-        if (coins[i] <= amount) {
-            take = 1 + solve(i, amount - coins[i], coins);
-        }
-        return t[i][amount] = min(take, nottake);
-    }
+    //     if (i == 0) {
+    //         if (amount % coins[0] == 0) {
+    //             return amount / coins[0];
+    //         }
+    //         return INF;
+    //     }
+    //     if (t[i][amount] != -1)
+    //         return t[i][amount];
+    //     int take = INF;
+    //     int nottake = solve(i - 1, amount, coins);
+    //     if (coins[i] <= amount) {
+    //         take = 1 + solve(i, amount - coins[i], coins);
+    //     }
+    //     return t[i][amount] = min(take, nottake);
+    // }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        memset(t, -1, sizeof(t));
-        int ans = solve(n - 1, amount, coins);
-        return ans == INF ? -1 : ans;
+        // memset(t, -1, sizeof(t));
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int a = 0; a <= amount; a++) {
+            if (a % coins[0] == 0)
+                dp[0][a] = a / coins[0];
+            else
+                dp[0][a] = INF;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int a = 0; a <= amount; a++) {
+                int take = INF;
+                int nottake = dp[i - 1][a];
+                if (coins[i] <= a) {
+                    take = 1 + dp[i][a - coins[i]];
+                }
+                dp[i][a] = min(take, nottake);
+            }
+        }
+        // int ans = solve(n - 1, amount, coins);
+        // return ans == INF ? -1 : ans;
+        return (dp[n - 1][amount] == INF ? -1 : dp[n - 1][amount]);
     }
 };
