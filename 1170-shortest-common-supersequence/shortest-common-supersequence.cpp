3a04f128e87a90f1e6fd0971d3b23bb03bccdb46
Shaurya Aditya Verma <143933774+shauryaadi99@users.noreply.github.com>
2026-09-06 01:21:29 +0530

    Time: 23 ms (88.63%) | Memory: 26.7 MB (90.69%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -3,45 +3,104 @@ public:
     string shortestCommonSupersequence(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
+
+        // dp[i][j] = length of LCS between:
+        // str1[0 ... i-1] and str2[0 ... j-1]
+        //
+        // dp[0][j] = 0 and dp[i][0] = 0 automatically
+        // because vector is initialized with 0.
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        
-        string res = "";
+
+        // -------------------- STEP 1: BUILD LCS TABLE --------------------
+        //
+        // We first find the LCS because characters belonging to the LCS
+        // can be shared between str1 and str2 in the supersequence.
+        //
+        // SCS length = n + m - LCS length
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
+
+                // Same character:
+                // This character can be used ONCE for both strings.
                 if (str1[i - 1] == str2[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
+                }
+
+                // Different characters:
+                // We cannot share them, so choose the direction
+                // which gives us the longer LCS.
+                else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        
+
+        // LCS length = dp[n][m]
+        // Therefore minimum possible SCS length is:
+        //
+        // n + m - LCS
         int ans = n + m - dp[n][m];
-        int i = n, j = m;
+
+        // ---------------- STEP 2: RECONSTRUCT SCS ----------------
+        //
+        // Start from the bottom-right of the LCS table and walk backwards.
+        int i = n;
+        int j = m;
+
+        string res = "";
+
         while (i > 0 && j > 0) {
+
+            // If characters are equal, this character belongs to
+            // the common subsequence.
+            //
+            // Add it only ONCE and move diagonally.
             if (str1[i - 1] == str2[j - 1]) {
                 res.push_back(str1[i - 1]);
                 i--;
                 j--;
-            } else {
+            }
+
+            else {
+
+                // We need to decide whether to take the character
+                // from str1 or str2.
+                //
+                // dp[i-1][j] > dp[i][j-1]
+                // means the LCS is better by moving UP,
+                // so take str1[i-1].
+                //
+                // Otherwise move LEFT and take str2[j-1].
+
                 if (dp[i - 1][j] < dp[i][j - 1]) {
                     res.push_back(str2[j - 1]);
                     j--;
-                } else {
+                }
+                else {
                     res.push_back(str1[i - 1]);
                     i--;
                 }
             }
         }
+
+        // If str2 is exhausted, all remaining characters
+        // of str1 must be included.
         while (i > 0) {
             res.push_back(str1[i - 1]);
             i--;
         }
+
+        // If str1 is exhausted, all remaining characters
+        // of str2 must be included.
         while (j > 0) {
             res.push_back(str2[j - 1]);
             j--;
         }
+
+        // We traversed from right -> left, so res is currently reversed.
         reverse(res.begin(), res.end());
+
         return res;
     }
 };
